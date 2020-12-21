81a37dcddd47ecc89dded91fcdba12aba99ea057
Michelle <mliuu3259@gmail.com>
2020-12-21 04:33:33 -0500

    added location selector and lat/long location conversions

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import './App.css';
+import "./App.css";
 import StoryList from "./components/storyList.js";
 import StorySubmit from "./components/storySubmit.js";
 import AdminConsole from "./components/adminConsole.js";
 
 function App() {
-    return (
-        <div className="App">
-            <h1>List of Stories:</h1>
-            <StoryList />
-            <br></br>
-            <h1>Submit a Story:</h1>
-            <StorySubmit />
-            <h1>Admin Console Base Func:</h1>
-            <AdminConsole />
-        </div>
-    );
+  return (
+    <div className='App'>
+      <h1>List of Stories:</h1>
+      <StoryList />
+      <br></br>
+      <h1>Submit a Story:</h1>
+      <StorySubmit />
+      <h1>Admin Console Base Func:</h1>
+      <AdminConsole />
+    </div>
+  );
 }
 
 export default App;
