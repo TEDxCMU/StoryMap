14e37b373a7e27261dd3f682d2b87c58dc2d682b
Bryce <bryce.d.li@gmail.com>
2020-12-22 10:25:14 -0800

    now displays firebase items on markers. for some reason it only displays two items instead of three.

--- src/components/storyMapView.js
+++ src/components/storyMapView.js
@@ -1,11 +1,22 @@
 import { useState, useEffect } from "react";
 import StoryService from "../services/story.service";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import L from 'leaflet';
 // import "leaflet/dist/leaflet.css";
-
+// import MarkerImage from "../public/logo192.png";
 const position = [51.505, -0.09]
 
 export default function StoryMapView() {
+    const markerIcon = L.icon({
+        iconUrl: process.env.PUBLIC_URL + '/marker.svg',
+        // shadowUrl: 'leaf-shadow.png',
+    
+        iconSize:     [32, 32], // size of the icon
+        // shadowSize:   [50, 64], // size of the shadow
+        iconAnchor:   [16, 32], // point of the icon which will correspond to marker's location
+        // shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor:  [0, -40] // point from which the popup should open relative to the iconAnchor
+    });
     const [allStories, setAllStories] = useState([]);
 
     useEffect(() => {
@@ -18,12 +29,15 @@ export default function StoryMapView() {
         .then(response => {
             response.docs.forEach(document => {
                 if (document.data().approved) {
+                    const pos = [document.data().latLong.lat, document.data().latLong.lng];
                     const fetchedStory = {
                         id: document.id,
                         name: document.data().name,
-                        storyText: document.data().story.text
+                        storyText: document.data().story.text,
+                        latLong: pos
                     };
-                    console.log(document);
+                    console.log(fetchedStory.latLong);
+
                     fetchedStories.push(fetchedStory);
                 }
             });
@@ -35,16 +49,21 @@ export default function StoryMapView() {
     return (
         <div>
 
-            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+            <MapContainer center={position} zoom={1} scrollWheelZoom={false}>
                 <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={position}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-                </Marker>
+                {allStories?.map(({ id, name, storyText, latLong}, index) => (
+                    <Marker position={latLong} key={index} icon={markerIcon}>
+                    <Popup>
+                        {name} <br/> {storyText}
+                    </Popup>
+                    </Marker>
+                    
+                ))}
+
+                
             </MapContainer>
         </div>
     )
