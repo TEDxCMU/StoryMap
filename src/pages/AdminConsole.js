e6546420d727b01f10fdafb5c4b661686b13c6e7
Neeharika V <neeharules@gmail.com>
2021-01-11 00:24:39 -0800

    make footer sticky

--- src/pages/AdminConsole.js
+++ src/pages/AdminConsole.js
@@ -2,13 +2,23 @@ import AdminConsole from '../components/AdminConsole/AdminConsole';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
  
+const container = {
+    position: 'relative',
+    minHeight: '100vh'
+};
+
+const content = {
+    paddingBottom: '6rem'
+};
 
 export default function LoginPage() {
     return (
-        <>
-            <Header />
-            <AdminConsole />
+        <div style={container}>
+            <div style={content}>
+                <Header />
+                <AdminConsole />
+            </div>
             <Footer />
-        </>
+        </div>
     );
 }
