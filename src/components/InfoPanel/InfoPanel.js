27c2b31622a1f34bff5b96a96625e6b40818ba45
Bryce <bryce.d.li@gmail.com>
2021-02-11 16:49:28 -0500

    Changed some messages to reflect Salon stuff

--- src/components/InfoPanel/InfoPanel.js
+++ src/components/InfoPanel/InfoPanel.js
@@ -5,7 +5,7 @@ export default function InfoPanel() {
         <div className={styles.info_body}>
             <div className={styles.info_content}>
                 <p className={styles.info_title}><span className={styles.info_fancy}>STORIES</span> GLOBALLY.</p>
-                <p className={styles.info_text}>Members of the CMU community around the world share their stories geographically.</p>
+                <p className={styles.info_text}>A virtual exploration of Carnegie Mellon's geographic and cultural diversity.</p>
             </div>
         </div>
     )
