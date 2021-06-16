0a6a3e25340e975255d4df23742e8422db7c65be
Kazi Jawad <kazi.a.jawad@gmail.com>
2021-06-16 12:28:25 -0400

    update Info text

--- src/components/InfoPanel/InfoPanel.js
+++ src/components/InfoPanel/InfoPanel.js
@@ -5,8 +5,13 @@ function InfoPanel() {
         <div className={styles.info_body}>
             <div className={styles.info_content}>
                 <p className={styles.info_title}>GLOBAL <span className={styles.info_fancy}>NARRATIVES</span>.</p>
-                <p className={styles.info_text}>A virtual exploration of Carnegie Mellon's geographic and cultural diversity. <br/> <i> click on markers to see a story <br/> click on the map to start sharing your story </i> </p>
-                {/* <p className={styles.info_text}>hover over markers to see a story <br/> click anywhere to start sharing your story</p> */}
+                <p className={styles.info_text}>
+                    A virtual exploration of Carnegie Mellon's geographic and cultural diversity.
+                    <br/>
+                    <i>Click on markers to see a story.</i>
+                    <br/>
+                    <i>Click on the map to start sharing your story.</i>
+                </p>
             </div>
         </div>
     )
