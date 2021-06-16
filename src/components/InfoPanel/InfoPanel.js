d7c606218d38f889aebae357f4487cef13b1863c
Bryce <bryce.d.li@gmail.com>
2021-06-15 21:35:10 -0700

    Updated helper text

--- src/components/InfoPanel/InfoPanel.js
+++ src/components/InfoPanel/InfoPanel.js
@@ -5,7 +5,8 @@ export default function InfoPanel() {
         <div className={styles.info_body}>
             <div className={styles.info_content}>
                 <p className={styles.info_title}>GLOBAL <span className={styles.info_fancy}>NARRATIVES</span>.</p>
-                <p className={styles.info_text}>A virtual exploration of Carnegie Mellon's geographic and cultural diversity.</p>
+                <p className={styles.info_text}>A virtual exploration of Carnegie Mellon's geographic and cultural diversity. <br/> <i> click on markers to see a story <br/> click on the map to start sharing your story </i> </p>
+                {/* <p className={styles.info_text}>hover over markers to see a story <br/> click anywhere to start sharing your story</p> */}
             </div>
         </div>
     )
