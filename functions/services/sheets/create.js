1ea8e83133652d5595850f850533857a9cc929d1
Kazi Jawad <kazi.a.jawad@gmail.com>
2021-07-17 12:15:32 -0400

    setup sheets in functions

--- functions/services/sheets/create.js
+++ functions/services/sheets/create.js
@@ -1,5 +1,25 @@
-module.exports = (snapshot, context) => {
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const serviceAccount = require('../../config/tedxcmu-narratives.json');
+
+const document = new GoogleSpreadsheet('1-Ei_YyqTYnRngVS9po_fcxaPer-5JyOmU989_li0mCs');
+
+// Shell Teast: sheets.create({ name: 'Kazi', email: 'kazi.a.jawad@gmail.com', prompt: 'Hi!', story: { text: 'Test' }, latLong: { lat: 44.8745685296278, lng: -101.02407217025758 } });
+module.exports = async (snapshot, context) => {
     const newValue = snapshot.data();
-    console.log(newValue);
-    // TODO: Send To Google Sheets
+
+    await document.useServiceAccountAuth(serviceAccount);
+    await document.loadInfo();
+
+    const sheet = document.sheetsByIndex[0];
+    await sheet.addRow([
+        newValue.name,
+        newValue.email,
+        newValue.prompt,
+        newValue.story?.text,
+        `${newValue.latLong?.lat}, ${newValue.latLong?.lng}`,
+        'PENDING',
+        'PENDING'
+    ]);
+
+    return;
 };
--- functions/services/sheets/update.js
+++ functions/services/sheets/update.js
@@ -1,9 +1,27 @@
-module.exports = (change, context) => {
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const serviceAccount = require('../../config/tedxcmu-narratives.json');
+
+const document = new GoogleSpreadsheet('1-Ei_YyqTYnRngVS9po_fcxaPer-5JyOmU989_li0mCs');
+
+module.exports = async (change, context) => {
     const previousValue = change.before.data();
     const newValue = change.after.data();
 
+    await document.useServiceAccountAuth(serviceAccount);
+    await document.loadInfo();
+
     if (!previousValue.approved && newValue.approved) {
-        console.log('Approved');
-        // TODO: Update Google Sheets
+        const sheet = document.sheetsByIndex[0];
+        await sheet.addRow([
+            newValue.name,
+            newValue.email,
+            newValue.prompt,
+            newValue.story?.text,
+            `${newValue.latLong?.lat}, ${newValue.latLong?.lng}`,
+            'APPROVED',
+            'PENDING'
+        ]);
     }
+
+    return;
 };
