16a49847f3551ad5917b2c5c01859f878ca8bd7d
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2022-09-17 13:38:00 -0400

    update text for main and about page, minus innovation about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,7 +9,7 @@ function About() {
                     About
                 </h1>
                 <p className={styles.text}>
-                    Global Narratives is a joint venture by TEDxCMU Innovation and TEDxCMU Salon.
+                    I Know A Place is a joint venture by TEDxCMU Innovation and TEDxCMU Salon.
                 </p>
             </section>
             <section className={styles.block}>
@@ -17,7 +17,7 @@ function About() {
                     Salon
                 </h2>
                 <p className={styles.text}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    TEDxCMU Salon is a tight-knit team that uplifts student voices and strengthens our Carnegie Mellon community through self-curated events, projects, and gatherings outside of the annual TEDx mainstage conference. We strive for relevance to our current collective moment and impact on community. Feel free to engage with our past events, including Black Academics Matter, via <a href='https://www.tedxcmu.org/'>tedxcmu.org</a>.
                 </p>
             </section>
             <section className={styles.block}>
--- src/components/InfoPanel/InfoPanel.js
+++ src/components/InfoPanel/InfoPanel.js
@@ -3,7 +3,7 @@ import styles from './InfoPanel.module.css';
 function InfoPanel() {
     return (
         <section className={styles.container}>
-            <p className={styles.title}>GLOBAL <span className={styles.span}>NARRATIVES</span></p>
+            <p className={styles.title}><span className={styles.span}>I KNOW A PLACE</span></p>
             <p className={styles.body}>
                 A virtual exploration of Carnegie Mellon's geographic and cultural diversity.
                 To get started, click on a marker to see a story. To share your story click on any part of the map.
